acdac8c2ac0ed142c3a07c3c8de09a910fa6ae1a
Chuck Youngfeldt <chuck@enclaveclouds.com>
2024-09-18 00:00:31 -0400

    cleanup

--- nitro-attestation/target/release/att_doc_retriever_sample.d
+++
@@ -1,1 +0,0 @@
-/Users/fierce/workspace/enclave/test-enclave/nitro-attestation/target/release/att_doc_retriever_sample: /Users/fierce/workspace/enclave/test-enclave/nitro-attestation/src/main.rs
--- nitro-attestation/target/release/nitro-attestation.d
+++
@@ -1,1 +0,0 @@
-/Users/fierce/workspace/enclave/test-enclave/nitro-attestation/target/release/nitro-attestation: /Users/fierce/workspace/enclave/test-enclave/nitro-attestation/src/main.rs
